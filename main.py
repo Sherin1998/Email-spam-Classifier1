4de71dc8718826df497a4d01a2d10765caed3cf4
Sherin Shaji <75293746+Sherin1998@users.noreply.github.com>
2023-01-05 14:18:44 +0530

    code change

--- main.py
+++ main.py
@@ -34,20 +34,26 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
 
-input_sms = st.text_area("Enter the message")
+def main():
 
-if st.button('Predict'):
+    st.title("Email/SMS Spam Classifier")
 
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    input_sms = st.text_area("Enter the message")
+
+    if st.button('Predict'):
+
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+
+if __name__=="__main__":
+    main()
